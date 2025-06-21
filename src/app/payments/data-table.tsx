02eec1f728afd5b55b1a0e6acb92ab45f930a6dd
Diego <diegoalt.g@hotmail.com>
2025-06-20 23:28:01 -0400

    finalizando proyecto

--- src/app/payments/data-table.tsx
+++ src/app/payments/data-table.tsx
@@ -5,6 +5,9 @@ import {
   flexRender,
   getCoreRowModel,
   useReactTable,
+   getPaginationRowModel,
+    SortingState,
+     getSortedRowModel
 } from "@tanstack/react-table"
 
 import {
@@ -15,6 +18,9 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
+import { Button } from "@/components/ui/button"
+import { DataTablePagination } from "@/components/TablePagination"
+import * as React from "react"
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
@@ -25,13 +31,28 @@ export function DataTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+
+   const [sorting, setSorting] = React.useState<SortingState>([])
+   const [rowSelection,setRowSelection] = React.useState({})
+
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+        onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onRowSelectionChange:setRowSelection,
+    state: {
+      sorting,
+      rowSelection
+    },
   })
 
+  
   return (
+    <div>
+
     <div className="rounded-md border">
       <Table>
         <TableHeader>
@@ -43,9 +64,9 @@ export function DataTable<TData, TValue>({
                     {header.isPlaceholder
                       ? null
                       : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
                   </TableHead>
                 )
               })}
@@ -56,8 +77,8 @@ export function DataTable<TData, TValue>({
           {table.getRowModel().rows?.length ? (
             table.getRowModel().rows.map((row) => (
               <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
+              key={row.id}
+              data-state={row.getIsSelected() && "selected"}
               >
                 {row.getVisibleCells().map((cell) => (
                   <TableCell key={cell.id}>
@@ -76,5 +97,28 @@ export function DataTable<TData, TValue>({
         </TableBody>
       </Table>
     </div>
+    {/* <div className="flex items-center justify-end space-x-2 py-4">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
+      </div> */}
+      <div className="my-5">
+
+        <DataTablePagination table={table}/>
+      </div>
+          </div>
   )
 }
